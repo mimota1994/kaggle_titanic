4ae62738dbe573c61a4a46a6e6458201f718d842
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2018-03-07 15:24:24 +0800

    利用cv也就是交叉验证

--- titanic.py
+++ titanic.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression,LinearRegression
+from sklearn.model_selection import cross_val_predict
 
 csv_data=pd.read_csv(r'c:\Users\huang\Downloads\train.csv',
                 usecols=['Survived','Pclass','Sex','Age','SibSp','Parch','Fare'])
@@ -10,29 +11,30 @@ csv_data=csv_data.fillna(csv_data.mean())
 x=csv_data[['Pclass','Sex','Age','SibSp','Parch','Fare']]
 y=csv_data['Survived']
 
-x_train=np.array(x)[:-100]
-y_train=np.array(y)[:-100]
+x_train=np.array(x)
+y_train=np.array(y)
 
 x_test=np.array(x)[-100:]
 y_test=np.array(y)[-100:]
 
-linreg=LinearRegression()
-linreg.fit(x_train,y_train)
+#linreg=LinearRegression()
+#linreg.fit(x_train,y_train)
 
 logreg=LogisticRegression(C=10)
 #logreg.fit(x_train,y_train)
-logreg.fit(x,y)
+#logreg.fit(x,y)
 
-z=logreg.predict(x_test)
-print('log:',logreg.score(x_test,y_test))
-print('linear:',linreg.score(x_test,y_test))
+#z=logreg.predict(x_test)
+predicted=cross_val_predict(logreg,x_train,y_train,cv=10)
+#print('log:',logreg.score(x_test,y_test))
+#print('linear:',linreg.score(x_test,y_test))
 
 all_data=pd.read_csv(r'C:\Users\huang\Downloads\test.csv',
                       usecols=['PassengerId','Pclass','Sex','Age','SibSp','Parch','Fare'])
 test_data=all_data[['Pclass','Sex','Age','SibSp','Parch','Fare']]
 test_data=test_data.replace(['male','female'],[1,0])
 test_data=test_data.fillna(test_data.mean())
-print(type(logreg.predict(test_data)))
+#print(type(logreg.predict(test_data)))
 csv_in=all_data[['PassengerId']]
-csv_in['Survived']=pd.Series(logreg.predict(test_data))
-csv_in.to_csv(r'C:\Users\huang\Desktop\titanic2.csv',index=False)
+csv_in['Survived']=pd.Series(predicted)
+csv_in.to_csv(r'C:\Users\huang\Desktop\titanic3.csv',index=False)
